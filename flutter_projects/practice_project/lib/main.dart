c8c1d9e83e950de446718fc01044af7f63b8f38c
mayurshelar-coditas <mayur.shelar@coditas.com>
2024-03-08 16:05:02 +0530

    .

--- flutter_projects/practice_project/lib/main.dart
+++ flutter_projects/practice_project/lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Expende Tracker'),
+          title: const Text('Expense Tracker'),
           // toolbarHeight: 10,
           backgroundColor: Colors.blue,
         ),
